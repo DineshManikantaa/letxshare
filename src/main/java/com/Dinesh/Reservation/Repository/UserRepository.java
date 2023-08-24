5ada740544abd8beb7ba562101329d2e13ed018e
DineshManikantaa <142876558+DineshManikantaa@users.noreply.github.com>
2023-08-24 23:50:12 +0530

    Delete src directory

--- src/main/java/com/Dinesh/Reservation/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Dinesh.Reservation.Repository;
-
-import com.Dinesh.Reservation.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User,Long> {
-
-	public User findByUsername(String username);
-}
